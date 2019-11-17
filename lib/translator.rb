de14e137b539c6383212443dffcac31333ab42a1
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-11-17 12:35:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,9 @@ def get_japanese_emoticon(file_path, emoticon)
     library[:get_emoticon][emoticon]
   else
     return "Sorry, that emoticon was not found"
+  end
+  
+  
 end
 
 def get_english_meaning
