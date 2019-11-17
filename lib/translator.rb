d4af31857a1fd05590b2528629e45a40723a3546
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-11-17 12:29:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file_path)
   
   emoticons.each do |meaning, emoticon|
     new_hash[:get_meaning][emoticon[1]] = meaning
-    new_hash[:get_meaning][emoticon[1]] = meaning
+    new_hash[:get_emoticon][emoticon[1]] = meaning
     
   
 end
